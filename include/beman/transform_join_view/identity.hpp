039a2036a0d51b78991db9c97cae7eb422f1086d
jainsujan11 <jainsujan031@kgpian.iitkgp.ac.in>
2025-12-03 11:54:20 +0530

    Initial project setup for beman.transform_view_join

---
+++ include/beman/transform_join_view/identity.hpp
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#ifndef BEMAN_TRANSFORM_JOIN_VIEW_IDENTITY_HPP
+#define BEMAN_TRANSFORM_JOIN_VIEW_IDENTITY_HPP
+
+
+namespace beman::transform_join_view {
+
+
+} // namespace beman::transform_join_view
+
+#endif // BEMAN_TRANSFORM_JOIN_VIEW_IDENTITY_HPP
